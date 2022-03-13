5668894dfb20ba6c70e97da0eb458e8ba49031ec
moses <academicsnotes@gmail.com>
2022-03-13 13:38:43 +0800

    Commit script

--- Basic/functions/script.js
+++ Basic/functions/script.js
@@ -17,3 +17,58 @@ var calc = {
 }
 
 calc.plus(2,2);
+
+var Dog= function(){
+    var name, breed;
+    return console.dir(this);
+}
+
+var speak = function(saywhat){
+    console.log(saywhat);
+}
+
+var speaks = function(what){
+    // console.log(this.love);
+    // console.log(this.normal);
+    console.log(what);
+}
+
+var Cat= function(){
+    var name, breed;
+}
+
+var plus = function(){
+    var sum = 0;
+    for (var i = arguments.length -1; i >= 0; i--){
+        sum += arguments[i];
+    }
+    return sum;
+}
+
+
+Dog.prototype.speak = speak;
+Cat.prototype.speak= speak;
+
+firstDog = new Dog;
+firstDog.name = "Rover";
+firstDog.breed = "Doberman";
+firstDog.speak('woof');
+
+secondDog = new Dog;
+secondDog.name = "Fluffy";
+
+console.log(firstDog.name)
+console.log(secondDog.name)
+
+firstCat = new Cat;
+firstCat.name = "Lizzy";
+firstCat.breed = "Domestic Cat";
+firstCat.speak('meow');
+
+var saySomething = {normal: "meow", love: "purr"};
+speaks.call(saySomething, saySomething.normal);
+
+// Can pass in an array or element
+speaks.apply(saySomething, ['meouff']);
+
+console.log(plus(2,2,2,3,2,3,4))

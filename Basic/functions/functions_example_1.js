56f591a49e0a8edbc734fa82937bf02bfe2936ec
moses <academicsnotes@gmail.com>
2022-03-26 10:29:19 +0800

    commit scripts

--- Basic/functions/functions_example_1.js
+++ Basic/functions/functions_example_1.js
@@ -4,3 +4,26 @@ function plus(a,b){
 }
 
 console.log(plus(2,2));
+
+function product(a, b){
+    return a*b;
+}
+
+console.log(product(4,3));
+
+const bigProduct = product(1000, 10000);
+console.log(bigProduct);
+
+setTimeout(function() {
+    console.log("Hey! You rock!!!");
+}, 1000)
+
+
+// arrow functions
+// can drop {} and ; if only 1 line.
+// Note: Do not use => function to look fancy
+setTimeout(() => console.log("Hey! You rock!!!"), 1000);
+
+setTimeout(() => {
+    console.log("Hey! You rock!!!");
+}, 1000)

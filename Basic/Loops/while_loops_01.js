04ed75d5a8517694079db64b7e3c41af6bbc7ffa
moses <academicsnotes@gmail.com>
2022-03-26 23:54:58 +0800

    Commit scripts

---
+++ Basic/Loops/while_loops_01.js
@@ -0,0 +1,22 @@
+//  ============== While Loop Demo =============================
+
+// let password ="tomorrow";
+// let userGuess = "";
+
+// while(userGuess !== password){
+//     userGuess = prompt("Please enter your password");
+// }
+// alert("Login Successful.");
+
+// ==================== Do While Demo ======================
+
+let factorial = 1;
+let number = 5;
+let original = number;
+
+do{
+    factorial = factorial * number;
+    number--;
+}while(number > 0);
+
+console.log(original + " factorial is " + factorial);

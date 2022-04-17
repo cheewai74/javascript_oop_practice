8d6be89b21abac3b2c1b2ffaac8d1dbaeeebec42
moses <academicsnotes@gmail.com>
2022-04-17 22:03:54 +0800

    Commit scripts

--- OOP/class/app.js
+++ OOP/class/app.js
@@ -9,8 +9,16 @@ class Car{
     carStats(){
         return `This car has ${this.doors} doors, a ${this.engine} engine and a beautiful ${this.color} color!`
     }
+
+    static totalDoors(car1, car2){
+        const doors1 = car1.doors;
+        const doors2 = car2.doors;
+        return doors1 + doors2;
+    }
 }
 
 const cx5 = new Car(4, 'v6', 'grey');
+const cx6 = new Car(2, 'bm', 'blue');
 console.log(cx5);
 console.log(cx5.carStats());
+console.log(Car.totalDoors(cx5, cx6));

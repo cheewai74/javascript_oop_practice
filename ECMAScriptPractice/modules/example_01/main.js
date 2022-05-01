7680544500ad29973328dd0d47e4e9b6cf4d6d27
moses <academicsnotes@gmail.com>
2022-05-01 20:54:45 +0800

    Commit scripts

---
+++ ECMAScriptPractice/modules/example_01/main.js
@@ -0,0 +1,3 @@
+import {getRandomNumber, getSum} from './util.js';
+console.log(getRandomNumber());
+console.log(getSum(2,5));
---
+++ ECMAScriptPractice/modules/example_01/util.js
@@ -0,0 +1,9 @@
+function getRandomNumber(){
+    return Math.random();
+}
+
+function getSum(a, b){
+    return a + b;
+}
+
+export{getRandomNumber, getSum}

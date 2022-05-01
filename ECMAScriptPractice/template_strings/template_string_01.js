c54654a6228bb7de87eb119886dfaaf516783233
moses <academicsnotes@gmail.com>
2022-05-01 17:21:27 +0800

    Commit scripts

--- ECMAScriptPractice/template_strings/template_string_01.js
+++ ECMAScriptPractice/template_strings/template_string_01.js
@@ -12,24 +12,27 @@
 const firstName = 'gns';
 console.log(`Hello ${firstName}, How are you?`);
 
-var a = 10, b = 20;
-console.log(`Additional value for ${a} and ${b} is ${a+b}`);
-var user = {name: 'gns'};
-console.log(`Current User: ${user.name.toUpperCase()}.`)
+// var a = 10, b = 20;
+// console.log(`Additional value for ${a} and ${b} is ${a+b}`);
+// var user = {name: 'gns'};
+// console.log(`Current User: ${user.name.toUpperCase()}.`)
 
-// Note ... spread operators
+// // Note ... spread operators
+// function tagFun(strings, ...values){
+//     console.log(strings.raw[0]);
+// }
+// tagFun `string text line 1 \n string text line 2`;
+
+var a = 5;
+var b = 10;
 function tagFun(strings, ...values){
-    console.log(strings.raw[0]);
-}
-tagFun `string text line 1 \n string text line 2`;
-
-var a = 5, b = 10;
-function tagFuns(strings, ...values){
     console.log(strings[0]);
     console.log(strings[1]);
     console.log(strings[2]);
     console.log(values[0]);
     console.log(values[1]);
-    return "Bazinga!";
+    return "test";
 }
-tagFuns `Hello ${a + b} world ${a * b}`;
+
+tagFun `Hello ${a + b} world ${a * b}`;
+console.log(tagFun `Hello ${a + b} world ${a * b}`);
